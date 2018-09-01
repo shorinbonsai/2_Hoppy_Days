444d088b0a73181cda5f73da9503fb810333743e
shorinbonsai <shorinbonsai@gmail.com>
2018-08-31 21:52:52 -0400

    left/right movement fixes

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -5,9 +5,9 @@ const SPEED = 750
 var motion = Vector2()
 
 func _physics_process(delta):
-	if Input.is_action_pressed("ui_right"):
+	if Input.is_action_pressed("ui_right") and not Input.is_action_pressed("ui_left"):
 		motion.x = SPEED
-	elif Input.is_action_pressed("ui_left"):
+	elif Input.is_action_pressed("ui_left") and not Input.is_action_pressed("ui_right"):
 		motion.x = -SPEED
 	else:
 		motion.x = 0
